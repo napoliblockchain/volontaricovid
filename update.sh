b38ca6021c8ea4897a5204ebb4f7114d9a52b97f
Sergio Casizzone <jambtc@gmail.com>
2020-03-31 17:05:47 +0200

    update bash

--- update.sh
+++ update.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 echo Updating...
-if [ ! -d "/var/www/npay/assets" ]; then
-    mkdir /var/www/npay/assets
+if [ ! -d "/var/www/dali/assets" ]; then
+    mkdir /var/www/dali/assets
 fi
-if [ ! -d "/var/www/npay/protected/verbali" ]; then
-    mkdir /var/www/npay/protected/verbali
-fi
+
 git stash
 git pull
 
-chmod -R ugo+rwx /var/www/npay/assets/
-chmod -R ugo+rwx /var/www/npay/uploads/
-chmod -R ugo+rwx /var/www/napay/custom/
-chmod -R ugo+rwx /var/www/npay/protected/runtime/
-chmod -R ugo+rwx /var/www/npay/protected/privatekeys/
-chmod -R ugo+rwx /var/www/npay/protected/log/
-chmod -R ugo+rwx /var/www/npay/protected/verbali/
-chmod 755 /var/www/npay/protected/yiic
-chmod 755 /var/www/npay/update.sh
-chmod +x /var/www/npay/update.sh
+
+chown -R www-data:www-data /var/www/dali/assets/
+chown -R www-data:www-data /var/www/dali/protected/runtime/
+
+chmod 755 /var/www/dali/protected/yiic
+chmod 755 /var/www/dali/update.sh
+chmod +x /var/www/dali/update.sh
 
 echo Versioning...
 git rev-parse HEAD>version.txt
